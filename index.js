73fdbf3e00d7acc7f99bfd8f265c481413268428
Anna <whitrebbit@gmail.com>
2022-12-17 18:05:53 +0200

    ready for webSocket

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { readFileSync } = require("fs")
 const { createServer } = require("http")
-const { Server } = require("ws")
+
 
 const server = createServer(handleRequest)
 
@@ -11,19 +11,9 @@ const js = readFileSync("script.js")
 
 let text = ""
 
+
+
 server.listen(1344, () => console.log("server started at http://localhost:1344"))
-
-const wss = new Server({ port: 1455 })
-
-wss.on("connection", handleConnection)
-
-function handleConnection(ws){
-  ws.send("hello from server")
-
-  ws.on("message", (message)=>{
-    console.log(message.toString())
-  })
-}
 
 function handleRequest(request, response) {
   if (request.url == "/" || request.url == "/index.html") {
--- script.js
+++ script.js
@@ -2,22 +2,8 @@ const textarea = document.getElementById("textarea")
 
 textarea.addEventListener("change", handleChange)
 
-connect()
+function handleChange() {
+  const options = { method: "post", body: textarea.value }
 
-function connect(){
-  const ws = new WebSocket('ws://localhost:1455')
-
-  ws.onopen = ()=>{
-    ws.send("hi from client")
-  }
-
-  ws.onmessage = (e)=>{
-    console.log(e.data)
-  }
-}
-
-function handleChange(){
-  const options = {method: "post", body: textarea.value}
-  
   fetch("http://localhost:1344", options)
 }
