f17cdc1b370cfc61b559c7efbdd0e7cce297e6ee
Anna <whitrebbit@gmail.com>
2022-12-31 20:11:25 +0200

    fix wss

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const textarea = document.getElementById("textarea")
 
 textarea.addEventListener("input", handleChange)
 
-const ws = new WebSocket(`ws://${location.hostname}:${httpPort}`)
+const ws = new WebSocket(`ws${location.protocol.slice(4)}//${location.hostname}:${httpPort}`)
 
 ws.onmessage = msg => textarea.value = msg.data
 
